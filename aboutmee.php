897853598f259b8375afe35d6980fe715c25e805
Burhanudin Dwi Saputra <63339790+reckless1701@users.noreply.github.com>
2020-04-08 18:58:21 +0700

    menghapus alamat

--- aboutmee.php
+++ aboutmee.php
@@ -2,7 +2,6 @@
 echo "Nama : Burhanudin Dwi Saputra"
 echo "Kelas : XI RPL 2"
 echo "Sekolah : SMK TARUNA BHAKTI"
-echo "Alamat : jalan kenari V blok i2 no 2 Rt 002 Rw 013 komp. TNI AD"
 echo "Nomer Handphone : 08970164532"
 echo "Email : dwisputr7@gmail.com"
 ?>
